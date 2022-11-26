3e11be86537146579b60ee606e3d5eebd6a92975
Jayaseelan Anbazhagan <jayaseelan.anbazhagan@ace-alternatives.com>
2022-11-26 16:07:18 +0530

    First commit

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../types/Books';
+import { BooksService } from './books.service';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css'],
+})
+export class BooksComponent implements OnInit {
+  Books: Book[] = [];
+
+  constructor(private bookService: BooksService) {}
+
+  ngOnInit(): void {
+    this.Books = this.bookService.getBooks();
+  }
+
+  isShowing: boolean = true;
+
+  addTocard(book: Book) {}
+}
---
+++ src/app/books/books.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BooksService {
+  constructor() {}
+
+  getBooks() {
+    let books = [
+      {
+        name: 'Clean code',
+        author: 'Robert C. Matin',
+        image:
+          'https://th.bing.com/th/id/OIP.xDBG3sSH9LqCyuHLt_ESggHaJy?w=130&h=180&c=7&r=0&o=5&pid=1.7',
+        amount: 700,
+      },
+      {
+        name: 'The Pragmatic Programmer',
+        author: 'Andrew Hunt',
+        image:
+          'https://m.media-amazon.com/images/I/4163oCmhq+L._SX425_BO1,204,203,200_.jpg',
+        amount: 800,
+      },
+      {
+        name: 'Art of Computer Programming',
+        author: 'Donald John Fuller',
+        image:
+          'https://m.media-amazon.com/images/I/410vJZbAZGS._SY393_BO1,204,203,200_.jpg',
+        amount: 900,
+      },
+      {
+        name: 'Introduction to the Design and Analysis of Algorithms',
+        author: 'Anany Levitin',
+        image:
+          'https://m.media-amazon.com/images/I/51T300XLDpL._SX369_BO1,204,203,200_.jpg',
+        amount: 1800,
+      },
+    ];
+    return books
+  }
+}
